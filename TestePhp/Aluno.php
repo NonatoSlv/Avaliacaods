0ef7f65c757c88a52fa13d2b296d80fae3b0a4a9
NonatoSlv <andreynonato_slv@outlook.com>
2025-04-03 21:48:47 -0300

    Add files via upload

---
+++ TestePhp/Aluno.php
@@ -0,0 +1,26 @@
+<?php
+require_once "Pessoa.php";
+class Aluno extends Pessoa {
+    private $nome;
+    private $idade;
+
+    public function __construct($nome, $idade) {
+        $this->nome = $nome;
+        $this->idade = $idade;
+        parent::__construct($nome, $idade);
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function getIdade() {
+        return $this->idade;
+        
+    }
+    public function falar() {
+        echo "Perguntando<br>";
+}
+ }
+
+?>
